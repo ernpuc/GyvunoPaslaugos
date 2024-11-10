16eac937a128daede82720c60fe9fc60fe02a783
ernpuc <ernpuc@gmail.com>
2024-11-10 17:30:24 +0200

    Fixed registration, added default admin user
    Admin email: admin@gmail.com
    Admin password: Admin_123

--- PetServiceWebApplication/Models/ApplicationRoles.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace PetServiceWebApplication.Models
-{
-    public class ApplicationRole : IdentityRole
-    {
-        public string? Description { get; set; }
-    }
-}
