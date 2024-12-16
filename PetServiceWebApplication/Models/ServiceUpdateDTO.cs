d4ea4c9c9d9b964134219c4874da82dbb0134096
ernpuc <ernpuc@gmail.com>
2024-12-16 16:33:31 +0200

    Added functionality for creating and updating services

---
+++ PetServiceWebApplication/Models/ServiceUpdateDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace PetServiceWebApplication.Models
+{
+    public class ServiceUpdateDTO
+    {
+        public Service Service { get; set; }
+        public PetServiceProvider.ProviderCategory Category { get; set; }
+    }
+}
