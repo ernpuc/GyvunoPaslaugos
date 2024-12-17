98f5fb49e105e61be3e8d8dee7b567c22c8a2674
ernpuc <ernpuc@gmail.com>
2024-12-17 21:37:43 +0200

    Fixed uploading images

---
+++ PetServiceWebApplication/Models/ProviderImageDTO.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace PetServiceWebApplication.Models
+{
+    public class ProviderImageDTO
+    {
+        public PetServiceProvider Provider { get; set; }
+        public string? ImageUrl { get; set; }
+        public IFormFile? ImageFile { get; set; }
+    }
+}
